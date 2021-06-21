bd8c5a2739cdbd92b996ffd55a42f9a3f540736a
nmastroianni <suancho@gmail.com>
2021-06-21 16:35:50 -0400

    fixing ignite layout

--- src/pages/ignite.js
+++ src/pages/ignite.js
@@ -7,15 +7,17 @@ import { HiSun } from "react-icons/hi";
 export default function Ignite() {
   return (
     <>
-      <Seo title="Ignite" />
+      <Seo title='Ignite' />
       <Layout>
-        <header className="mb-2 sm:mb-4 lg:mb-6 flex flex-col items-center">
-          <PageTitle title="Ignite">
-            Perhaps one of these ignites will be the start of something great
-            for you and your students.
-          </PageTitle>
-          <HiSun className="text-3xl sm:text-5xl lg:text-6xl text-blue-700 mt-3" />
-        </header>
+        <div className='max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 mb-12'>
+          <header className='mb-2 sm:mb-4 lg:mb-6 flex flex-col items-center'>
+            <PageTitle title='Ignite'>
+              Perhaps one of these ignites will be the start of something great
+              for you and your students.
+            </PageTitle>
+            <HiSun className='text-3xl sm:text-5xl lg:text-6xl text-blue-700 mt-3' />
+          </header>
+        </div>
       </Layout>
     </>
   );
