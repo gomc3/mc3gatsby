9159defaf859b6aa2a464e774f345526307eda8b
nmastroianni <suancho@gmail.com>
2021-06-30 18:03:11 -0400

    customizing 404 layout and styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,39 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import PageTitle from "../components/page-title";
+import { Link } from "gatsby";
+import Thinker from "../components/thinker";
+import { HiDocumentSearch } from "react-icons/hi";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+export default function NotFoundPage({ location: { pathname } }) {
+  console.log(pathname);
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    <Layout path={pathname}>
+      <Seo title='Resource Not Found' />
+      <div className='max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 mb-12'>
+        <header className='mb-2 sm:mb-4 lg:mb-6 flex flex-col items-center'>
+          <PageTitle
+            title='Resource Not Found'
+            icon={
+              <HiDocumentSearch className='text-3xl sm:text-5xl lg:text-6xl text-blue-700 mt-0 inline-block' />
+            }
+          />
+        </header>
+        <div className='max-w-md mx-auto flex flex-col justify-center items-center'>
+          <p className='text-3xl font-semibold text-blue-700 text-center'>
+            We're sorry! <br />
+            We can't seem to find what you were looking for.
+          </p>
+          <Link
+            to='/'
+            className='block mx-auto mt-6 text-center items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:border-blue-600 focus:shadow-outline-blue active:bg-blue-600 transition ease-in-out duration-150'
+          >
+            Visit Our Homepage
+          </Link>
+          <Thinker styles='w-full' />
+        </div>
+      </div>
+    </Layout>
+  );
 }
-
-export default NotFoundPage
