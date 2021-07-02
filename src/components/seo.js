e981daaa4dcd31836be542967aeef930cbdb9bed
nmastroianni <suancho@gmail.com>
2021-07-02 10:46:50 -0400

    removing Google font for performance

--- src/components/seo.js
+++ src/components/seo.js
@@ -81,12 +81,6 @@ export default function Seo({
         },
       ]}
     >
-      <link rel='preconnect' href='https://fonts.googleapis.com' />
-      <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
-      <link
-        href='https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;500;600;700&display=swap'
-        rel='stylesheet'
-      />
       {children}
     </Helmet>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,6 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Montserrat"],
-      },
       zIndex: {
         "-10": "-10",
       },
