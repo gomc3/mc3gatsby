fdffd99c08701142f82fe2547b341586c05354da
nmastroianni <suancho@gmail.com>
2021-07-03 06:58:42 -0400

    adding image link and wrapping time

--- src/components/blog-card.js
+++ src/components/blog-card.js
@@ -14,38 +14,43 @@ export default function BlogCard({
   },
 }) {
   return (
-    <div className="rounded-md shadow-md my-2 sm:my-3 lg:my-4 bg-gray-100 h-full">
+    <div className='rounded-md shadow-md my-2 sm:my-3 lg:my-4 bg-gray-100 h-full'>
       {cover ? (
-        <aside className="relative rounded-tl-md rounded-tr-md text-white h-40 sm:h-48 md:h-56 lg:h-60 w-full text-center overflow-hidden">
-          <GatsbyImage
-            image={getImage(cover.image)}
-            className="inset-0 z-0 rounded-tl-md rounded-tr-md"
-            alt={cover.alt}
-            title={cover.title}
-          />
+        <aside className='relative rounded-tl-md rounded-tr-md text-white h-40 sm:h-48 md:h-56 lg:h-60 w-full text-center overflow-hidden'>
+          <Link
+            to={slug}
+            className='no-underline hover:underline hover:text-blue-800'
+          >
+            <GatsbyImage
+              image={getImage(cover.image)}
+              className='inset-0 z-0 rounded-tl-md rounded-tr-md'
+              alt={cover.alt}
+              title={cover.title}
+            />
+          </Link>
         </aside>
       ) : (
         <></>
       )}
 
-      <section className="px-2 sm:px-3 lg:px-4 h-full">
-        <header className="flex justify-between items-center flex-1">
+      <section className='px-2 sm:px-3 lg:px-4 h-full'>
+        <header className='flex justify-between items-center flex-wrap'>
           <Link
             to={slug}
-            className="no-underline hover:underline hover:text-blue-800"
+            className='no-underline hover:underline hover:text-blue-800'
           >
-            <h1 className="text-2xl  my-1 sm:my-2 md:my-3 text-blue-800">
+            <h1 className='text-2xl  my-1 sm:my-2 md:my-3 text-blue-800'>
               {name}
             </h1>
           </Link>
 
-          <p className="text-sm text-blue-600">
-            <Link to={slug} className="underline hover:no-underline">
+          <p className='text-sm text-blue-600 flex-wrap'>
+            <Link to={slug} className='underline hover:no-underline'>
               {timeToRead} minute read
             </Link>
           </p>
         </header>
-        <div className="text-gray-700 text-sm sm:text-xs">
+        <div className='text-gray-700 text-sm sm:text-xs'>
           Last Updated{" "}
           {new Date(modifiedTime).toLocaleDateString("en-US", {
             month: "short",
@@ -53,10 +58,10 @@ export default function BlogCard({
             year: "numeric",
           })}
         </div>
-        <p className="text-base mt-2 sm:mt-3 md:mt-4 lg:mt-5 mb-6">{excerpt}</p>
+        <p className='text-base mt-2 sm:mt-3 md:mt-4 lg:mt-5 mb-6'>{excerpt}</p>
         <Link
           to={slug}
-          className="px-5 py-3 text-base font-medium rounded-md text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:shadow-outline transition duration-500 ease-in-out"
+          className='px-5 py-3 text-base font-medium rounded-md text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:shadow-outline transition duration-500 ease-in-out'
         >
           Continue to Full Article
         </Link>
