7418f2a0381ea1c77d59c2e7912dc6e45ad37f03
nmastroianni <suancho@gmail.com>
2021-07-09 04:55:33 -0400

    exec members vertical small screens

--- src/components/exec-member.js
+++ src/components/exec-member.js
@@ -11,29 +11,29 @@ export default function execMember({
 }) {
   return (
     <>
-      <div className="p-4 flex flex-wrap items-center">
+      <div className='p-4 flex flex-col text-center md:text-left md:flex-row flex-wrap items-center'>
         <a href={socialMediaLink}>
           {socialMediaImage ? (
             <GatsbyImage
               image={getImage(socialMediaImage.gatsbyImageData)}
-              alt=""
-              className="filter grayscale transition duration-500 ease-in-out hover:grayscale-0 w-24 mr-4 rounded-full flex-initial border border-gray-300"
+              alt=''
+              className='filter grayscale transition duration-500 ease-in-out hover:grayscale-0 w-24 md:mr-4 rounded-full flex-initial border border-gray-300'
             />
           ) : (
             <img
-              src="https://via.placeholder.com/100"
+              src='https://via.placeholder.com/100'
               alt={`placeholder for portrait of ${fullName}`}
-              className="w-24 mr-4 rounded-full flex-initial border border-gray-300"
+              className='w-24 md:mr-4 rounded-full flex-initial border border-gray-300'
             />
           )}
         </a>
-        <div className="flex-1">
-          <h3 className="text-xl font-medium">{fullName}</h3>
-          <h4 className="text-sm ">{role}</h4>
+        <div className='flex-1'>
+          <h3 className='text-xl font-medium'>{fullName}</h3>
+          <h4 className='text-sm '>{role}</h4>
           <p className={`${!socialMediaLink && `hidden`}`}>
             <a href={socialMediaLink}>
-              <FaLinkedin className="inline text-blue-700 text-sm" />
-              <span className="ml-2 text-xs">Connect on LinkedIn</span>
+              <FaLinkedin className='inline text-blue-700 text-sm' />
+              <span className='ml-2 text-xs'>Connect on LinkedIn</span>
             </a>
           </p>
         </div>
