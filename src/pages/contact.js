a011baae305d29b62eee0ee11637602fa00854f6
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-07-13 09:41:18 -0400

    making form max width wider

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -62,26 +62,26 @@ export default function Contact({ path }) {
   };
   return (
     <Layout path={path}>
-      <Seo title='Contact Us' />
-      <div className='max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 mb-12'>
-        <header className='mb-2 sm:mb-4 lg:mb-6 flex flex-col items-center'>
+      <Seo title="Contact Us" />
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 mb-12">
+        <header className="mb-2 sm:mb-4 lg:mb-6 flex flex-col items-center">
           <div>
             <PageTitle
-              title='Contact Us'
+              title="Contact Us"
               icon={
-                <HiChatAlt2 className='text-3xl sm:text-5xl lg:text-6xl mt-0 inline-block' />
+                <HiChatAlt2 className="text-3xl sm:text-5xl lg:text-6xl mt-0 inline-block" />
               }
             />
           </div>
-          <h2 className='text-lg text-gray-700 max-w-screen-sm'>
+          <h2 className="text-lg text-gray-700 max-w-screen-sm">
             Would you like to volunteer to do an Ignite Presentation, or do you
             have a question for us?
           </h2>
         </header>
         <hr />
-        <section className='max-w-screen-sm mx-auto my-3 sm:my-4 md:my-5 lg:my-6'>
+        <section className="max-w-screen-sm mx-auto my-3 sm:my-4 md:my-5 lg:my-6">
           {recaptchaPassed === false && (
-            <p className='text-center text-red-600 text-xl border border-red-600 p-4 rounded-md'>
+            <p className="text-center text-red-600 text-xl border border-red-600 p-4 rounded-md">
               Oops! It looks like Google blocked your submission because it
               thinks you are a robot. Your submission was not sent to us.
             </p>
@@ -89,64 +89,64 @@ export default function Contact({ path }) {
           {formComplete && (
             <button
               onClick={() => setFormComplete(!formComplete)}
-              className='block mx-auto text-center items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:border-blue-600 focus:shadow-outline-blue active:bg-blue-600 transition ease-in-out duration-150'
+              className="block mx-auto text-center items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:border-blue-600 focus:shadow-outline-blue active:bg-blue-600 transition ease-in-out duration-150"
             >
               Contact Us Again
             </button>
           )}
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className={`${formComplete && `hidden`}`}
+            className={`${formComplete && `hidden`} max-w-lg mx-auto`}
           >
             <label
-              htmlFor='name'
-              className='font-semibold text-lg text-blue-800'
+              htmlFor="name"
+              className="font-semibold text-lg text-blue-800 block"
             >
               What is your name?
             </label>
             <input
-              name='name'
-              type='text'
-              placeholder='Example: Jane Appleseed'
+              name="name"
+              type="text"
+              placeholder="Example: Jane Appleseed"
               {...register("name", {
                 required: "Your name is required.",
               })}
-              className='form-input max-w-sm block w-full mb-3 px-0.5 font-medium border-b-2 border-gray-200 focus:outline-none focus:border-blue-500'
+              className="form-input block w-full mb-3 px-0.5 font-medium border-b-2 border-gray-200 focus:outline-none focus:border-blue-500"
             />
             {errors.name && (
-              <p className='text-red-700'> &uarr; {errors.name.message}</p>
+              <p className="text-red-700"> &uarr; {errors.name.message}</p>
             )}
             <label
-              htmlFor='email'
-              className='font-semibold text-lg text-blue-800'
+              htmlFor="email"
+              className="font-semibold text-lg text-blue-800"
             >
               What is your email address?
             </label>
             <input
-              name='email'
-              type='email'
-              placeholder='Example: jappleseed@gomc3.org'
+              name="email"
+              type="email"
+              placeholder="Example: jappleseed@gomc3.org"
               {...register("email", {
                 required: "Your email address is required.",
               })}
-              className='form-input max-w-sm block w-full mb-3 px-0.5 font-medium border-b-2 border-gray-200 focus:outline-none focus:border-blue-500'
+              className="form-input block w-full mb-3 px-0.5 font-medium border-b-2 border-gray-200 focus:outline-none focus:border-blue-500"
             />
             {errors.email && (
-              <p className='text-red-700'> &uarr; {errors.email.message}</p>
+              <p className="text-red-700"> &uarr; {errors.email.message}</p>
             )}
             <label
-              htmlFor='reason'
-              className='font-semibold text-lg text-blue-800'
+              htmlFor="reason"
+              className="font-semibold text-lg text-blue-800"
             >
               What is your reason for contacting us today?
             </label>
             <select
-              name='reason'
+              name="reason"
               {...register("reason", {
                 required: "A reason is required.",
                 pattern: "^((?!Select).)*$",
               })}
-              className='form-select text-gray-500 max-w-sm block w-full mb-3 px-0.5 font-medium border-b-2 border-gray-200 focus:outline-none focus:border-blue-500'
+              className="form-select text-gray-500 block w-full mb-3 px-0.5 font-medium border-b-2 border-gray-200 focus:outline-none focus:border-blue-500"
             >
               {selectReason.map((reason, i) => (
                 <option key={reason} value={reason}>
@@ -155,30 +155,30 @@ export default function Contact({ path }) {
               ))}
             </select>
             <label
-              htmlFor='question'
-              className='font-semibold text-lg text-blue-800'
+              htmlFor="question"
+              className="font-semibold text-lg text-blue-800"
             >
               How can we help you?
             </label>
             <textarea
-              name='question'
-              placeholder='Enter your question or comment here...'
-              className='form-textarea max-w-sm block w-full mb-3 px-0.5 font-medium border-b-2 border-gray-200 focus:outline-none focus:border-blue-500'
+              name="question"
+              placeholder="Enter your question or comment here..."
+              className="form-textarea block w-full mb-3 px-0.5 font-medium border-b-2 border-gray-200 focus:outline-none focus:border-blue-500"
               {...register("question", {
                 required: "Your question/comment is required.",
               })}
             />
             {errors.question && (
-              <p className='text-red-700'> &uarr; {errors.question.message}</p>
+              <p className="text-red-700"> &uarr; {errors.question.message}</p>
             )}
             <ReCAPTCHA
               sitekey={`6LeIMHIbAAAAAF-Eu5prLZNWXnwaadSsV8OYN1mP`}
-              size='invisible'
+              size="invisible"
               ref={reRef}
             />
             <input
-              name='submit'
-              type='submit'
+              name="submit"
+              type="submit"
               className={`w-1/3 px-6 py-3 mt-6 font-medium rounded-md text-white bg-blue-700 ${
                 disabled && ` opacity-40 text-gray-50 `
               } transition duration-300 hover:bg-blue-800`}
