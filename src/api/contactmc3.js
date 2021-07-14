ee14ce512aa0debf2ff60957e8c0afd277491932
nmastroianni <suancho@gmail.com>
2021-07-13 21:25:22 -0400

    adding trello integration

--- src/api/contactmc3.js
+++ src/api/contactmc3.js
@@ -1,4 +1,5 @@
 const { google } = require("googleapis");
+import fetch from "node-fetch";
 import axios from "axios";
 
 export default async function formHandler(req, res) {
@@ -34,6 +35,57 @@ export default async function formHandler(req, res) {
         }
       });
       const data = [[timeStamp, name, email, reason, question]];
+      let today = new Date();
+      const listEmails = {
+        "Agenda Item": ["60ee3b02a70c66502f74972a", "60ee3b0210af375d669d7c4d"],
+        Billing: ["60ee3b0290b94052ef2511c0", "60ee3b0303303083d5a2aaf9"],
+        "Professional Development": [
+          "60ee3b02a70c66502f74972a",
+          "60ee3b0210af375d669d7c4d",
+        ],
+        "Volunteer to Write a Blog Post": [
+          "60ee3b02a70c66502f74972a",
+          "60ee3b0210af375d669d7c4d",
+        ],
+        "Volunteer to Present": [
+          "60ee3b02a70c66502f74972a",
+          "60ee3b0210af375d669d7c4d",
+        ],
+        Website: ["60ed997371e05577c0bb2ec9"],
+        "Other...": ["60ee3b02a70c66502f74972a", "60ee3b0210af375d669d7c4d"],
+      };
+      const listIds = {
+        "Agenda Item": "60ed99f9efb5dc10411d6351",
+        Billing: "60ed99f9efb5dc10411d6352",
+        "Professional Development": "60ed99f9efb5dc10411d6353",
+        "Volunteer to Write a Blog Post": "60ed9a480120b927354896af",
+        "Volunteer to Present": "60ed9a7e70f26f2c734c60b6",
+        Website: "60ed9a8a0a7f247611069009",
+        "Other...": "60ed9a8e7b89df0e1ca5cef1",
+      };
+      axios
+        .post(
+          `https://api.trello.com/1/cards?key=${process.env.TRELLO_API}&token=${process.env.TRELLO_TOKEN}&idList=${listIds[reason]}`,
+          {
+            name: `Question from ${name}: ${email}`,
+            desc: question,
+            pos: "bottom",
+            idMembers: listEmails[reason],
+            due: `${today.setDate(today.getDate() + 2)}`,
+            idLabels: ["60ed99f9c824613830751364"],
+          },
+          {
+            method: "POST",
+          }
+        )
+        .then((response) => {
+          //console.log(`Response: ${response.status} ${response.statusText}`);
+          return JSON.stringify(response.data);
+        })
+        //.then((text) => console.log(text))
+        .catch((err) => {
+          console.error(err);
+        });
       async function gsrun(client) {
         const gsapi = google.sheets({ version: "v4", auth: client });
         const request = {
