65951f5bf853816897f562df0d9dfcf761e691ef
nmastroianni <suancho@gmail.com>
2021-07-30 07:58:52 -0400

    adding return cue for skip navigation

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import YouTube from "./youtube";
 import AudioFile from "./audio";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import { BsArrowReturnLeft } from "react-icons/bs";
 
 export default function Layout({ children, path }) {
   const components = {
@@ -11,20 +12,24 @@ export default function Layout({ children, path }) {
     AudioFile: AudioFile,
   };
   return (
-    <MDXProvider components={components} className="relative">
-      <ul id="nav-access" className="relative mx-auto">
+    <MDXProvider components={components} className='relative'>
+      <ul id='nav-access' className='relative mx-auto'>
         <li>
           <a
-            href="#main-content"
-            className="absolute z-50 -top-20 sm:left-1/4 text-2xl text-blue-700 block w-full sm:w-1/2 text-center bg-blue-700 bg-opacity-10 transform focus:translate-y-20 transition-all duration-500 ease-in-out"
+            href='#main-content'
+            className='absolute z-50 -top-20 sm:left-1/4 text-2xl text-blue-700 inline-block w-full sm:w-1/2 text-center bg-blue-700 bg-opacity-10 transform focus:translate-y-20 transition-all duration-500 ease-in-out'
           >
-            Skip to main content
+            Skip to main content{" "}
+            <span className='text-white px-3 py-0 bg-blue-700 text-base rounded-sm'>
+              Return
+              <BsArrowReturnLeft className='w-3 h-3 inline text-white ml-1' />
+            </span>
           </a>
         </li>
       </ul>
-      <div className="flex flex-col min-h-screen space-between">
+      <div className='flex flex-col min-h-screen space-between'>
         <Navbar path={path} />
-        <main id="main-content">{children}</main>
+        <main id='main-content'>{children}</main>
         <Footer />
       </div>
     </MDXProvider>
