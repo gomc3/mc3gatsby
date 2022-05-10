3691595d2de887d0c68b68ac35ef0d4c9cda6611
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2022-05-10 13:23:02 -0400

    specifying captionSrc

--- src/components/audio.js
+++ src/components/audio.js
@@ -1,20 +1,25 @@
 import * as React from "react";
 
-export default function AudioFile({ title, id, captionSrc, lang }) {
+export default function AudioFile({
+  title,
+  id,
+  captionSrc = "https://drive.google.com/file/d/1HTNbqZE-jt-UTEZBZCp3fJSEszTQVALl/view?usp=sharing",
+  lang,
+}) {
   return (
-    <figure className='text-center'>
-      <figcaption className='text-lg font-semibold text-blue-700'>
+    <figure className="text-center">
+      <figcaption className="text-lg font-semibold text-blue-700">
         {title}
       </figcaption>
-      <audio controls className='mx-auto'>
+      <audio controls className="mx-auto">
         <source
           src={`https://docs.google.com/uc?id=${id}&export=open`}
-          type='audio/mp3'
+          type="audio/mp3"
         />
         <track
-          kind='captions'
+          kind="captions"
           srcLang={lang ? lang : `en`}
-          src='https://drive.google.com/file/d/1HTNbqZE-jt-UTEZBZCp3fJSEszTQVALl/view?usp=sharing'
+          src={captionSrc}
         ></track>
         If you see this, then your browser does not support the{" "}
         <code>audio</code> element.
